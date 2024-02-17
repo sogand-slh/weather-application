48b4f912b2b102fdcd6d19ba3fcc9754f31d1b72
Sogand.Saleh <sogand.saleh@partsoftware.com>
2024-02-17 17:38:05 +0330

    feat: implement use locale translate composable

--- src/composables/index.js
+++ src/composables/index.js
@@ -1,2 +1,3 @@
 export { default as useClassModifier } from "./useClassModifier.composable";
 export { default as useTimeout } from "./useTimeout.composable";
+export { default as useLocaleTranslator } from "./useLocaleTranslator.composable";
