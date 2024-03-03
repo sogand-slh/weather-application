397fe4ae2905a0352a2ce4cdad9dfd7a60344eef
Sogand.Saleh <sogand.saleh@partsoftware.com>
2024-03-03 17:21:06 +0330

    feat: implement parts of dashboard

---
+++ src/views/dashboard/dashboard.view.vue
@@ -0,0 +1,20 @@
+<template>
+  <base-card>
+    <div>
+      <image-loader name="logo" />
+      <span>{{ temperature }}</span>
+      <span>{{ temperature }}</span>
+    </div>
+    <div class="divider"></div>
+    <div>location</div>
+    <div>date</div>
+  </base-card>
+</template>
+
+<script setup>
+defineOptions({
+  name: "dashboard-view",
+});
+</script>
+
+<style lang="scss" scoped></style>
